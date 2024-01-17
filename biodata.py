4add16ae053fb48980da19e390e53283f55537c2
wahyoe <145647812+0nlyyouu@users.noreply.github.com>
2024-01-17 09:28:03 +0700

    Membuat biodata.py
    berisi kode input Biodata

---
+++ biodata.py
@@ -0,0 +1,8 @@
+Nama = input("Masukkan nama : ")
+Umur = input("Masukkan umur : ")
+Alamat = input("Masukkan alamat : ")
+
+print("\n---BIODATA---")
+print("Nama : " + Nama)
+print("Umur : " + Umur)
+print("Alamat : " + Alamat)
